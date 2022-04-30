dc8c0faae8e64458640217632e933c6117c04717
kaylendog <actuallyori@gmail.com>
2022-04-30 11:51:13 +0100

    feat: create basic fabric mod

---
+++ src/main/java/dog/kaylen/octarine/Octarine.java
@@ -0,0 +1,14 @@
+package dog.kaylen.octarine;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ExampleMod implements ModInitializer {
+    public static final Logger LOGGER = LoggerFactory.getLogger("octarine");
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info("Initializing Octarine...");
+    }
+}
