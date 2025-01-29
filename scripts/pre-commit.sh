760e8fab8811b5d6544c4c95740da3e36c9510c6
kaylendog <actuallyori@gmail.com>
2025-01-29 23:22:34 +0000

    chore: tidy up repository root

--- scripts/pre-commit.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-cd "$SCRIPT_DIR"/../../ || exit
-
-./gradlew spotlessApply
-
-git add $(git diff --diff-filter=ACMR --name-only --cached)
